36db8d5a3763c8520cb72b5ebb21192e82cbb816
Danny van der Sluijs <danny.vandersluijs@fleppuhstein.com>
2012-09-24 19:08:34 +0200

    Added requirments as requested.

--- src/ButtonPanel.java
+++ src/ButtonPanel.java
@@ -1,25 +1,56 @@
+
 import javax.swing.*;
 import java.awt.event.*;
+import java.awt.*;
 
 public class ButtonPanel extends JPanel implements ActionListener {
-  StopLicht sl;
-  JButton rood;
 
-  ButtonPanel(StopLicht sl) {
-    this.sl = sl;
-    rood = new JButton("rood");
-    add(rood);
-    rood.addActionListener(this);
-  }
+    StopLicht sl;
+    JButton rood;
+    Timer timerGroen, timerProgress;
+    JProgressBar progressBar;
 
-  public void actionPerformed(ActionEvent e) {
-    if (e.getSource() == rood) {
-      sl.springOp(StopLicht.ROOD);
-      try {
-        Thread.sleep(5000);
-      }
-      catch(InterruptedException ie) {}
-      sl.springOp(StopLicht.GROEN);
+    ButtonPanel(StopLicht sl) {
+        this.setLayout(new BorderLayout());
+        this.sl = sl;
+        
+        this.rood = new JButton("rood");
+        this.add(rood);
+        
+        this.rood.addActionListener(this);
+        
+        this.progressBar = new JProgressBar(0, 50);
+        this.add(this.progressBar, BorderLayout.NORTH);
     }
-  }
+
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == rood) {
+            sl.springOp(StopLicht.ROOD);
+            
+            /* Timer with 5 sec delay, to go back to green */
+            timerGroen = new Timer(5000, new TimerGroenAction());
+            timerGroen.start();
+            
+            timerProgress = new Timer(100, new TimerProgressAction());
+            timerProgress.start();
+        }
+    }
+          
+    class TimerGroenAction implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            sl.springOp(StopLicht.GROEN);
+            timerGroen.stop();
+        }        
+    }
+    
+    class TimerProgressAction implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            progressBar.setValue(progressBar.getValue() + 1);
+            
+            if (progressBar.getValue() == progressBar.getMaximum()) {
+                progressBar.setValue(0);
+                timerProgress.stop();
+            }
+        }        
+    }
 }
--- src/StopLicht.java
+++ src/StopLicht.java
@@ -1,36 +1,38 @@
+
 import javax.swing.*;
 import java.awt.*;
 
 public class StopLicht extends JPanel {
-  public final static int ROOD = 0;
-  public final static int GROEN = 1;
-  private int kleur = GROEN;
 
-  public StopLicht() {
-    setOpaque(false);
-    setPreferredSize(new Dimension(150, 200));
-  }
+    public final static int ROOD = 0;
+    public final static int GROEN = 1;
+    private int kleur = GROEN;
 
-  public void springOp(int i) {
-    kleur = i;
-    repaint();
-  }
-
-  public void paintComponent(Graphics g) {
-    super.paintComponent(g);
-    g.setColor(Color.GRAY);
-    int midden = getWidth() / 2;
-    int hoogte = getHeight();
-    g.fillRect(midden - 30, hoogte - 180, 60, 110);
-    g.fillRect(midden - 5, hoogte - 70, 10, 70);
-    switch(kleur) {
-      case ROOD:
-        g.setColor(Color.RED);
-        g.fillOval(midden - 20, hoogte - 170, 40, 40);
-        break;
-      case GROEN:
-        g.setColor(Color.GREEN);
-        g.fillOval(midden - 20, hoogte - 120, 40, 40);
+    public StopLicht() {
+        setOpaque(false);
+        setPreferredSize(new Dimension(150, 200));
     }
-  }
+
+    public void springOp(int i) {
+        kleur = i;
+        repaint();
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(Color.GRAY);
+        int midden = getWidth() / 2;
+        int hoogte = getHeight();
+        g.fillRect(midden - 30, hoogte - 180, 60, 110);
+        g.fillRect(midden - 5, hoogte - 70, 10, 70);
+        switch (kleur) {
+            case ROOD:
+                g.setColor(Color.RED);
+                g.fillOval(midden - 20, hoogte - 170, 40, 40);
+                break;
+            case GROEN:
+                g.setColor(Color.GREEN);
+                g.fillOval(midden - 20, hoogte - 120, 40, 40);
+        }
+    }
 }
